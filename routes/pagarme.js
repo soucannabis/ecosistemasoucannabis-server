ba68ae0fe465401aa91c71eb356383679bf5bae8
Cannabis <soucannabisadm@gmail.com>
2024-04-05 18:46:27 +0000

    First Commit Loja

---
+++ routes/pagarme.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+const express = require('express');
+const router = express.Router();
+const pagarmRequest = require('./modules/pagarmeRequest');
+const directusRequest = require('./modules/directusRequest');
+
+router.post('/orders', async (req, res) => {
+
+    const token = req.headers.authorization
+    const verToken = await directusRequest("/items/Users_Api?filter[token][_eq]=" + token + "", '', "GET")
+
+    if (verToken) {     
+        const order = await pagarmRequest("/orders", req.body, "POST")
+        console.log(order)
+        res.send(order)
+        res.status(200)
+    } else {
+        res.status(401).json({ mensagem: 'Credenciais inválidas' });
+        res.status(401)
+    }
+});
+
+module.exports = router;
