723cb6e965700221841451c90d266e029df190c5
SouCannabis <118570727+soucannabis@users.noreply.github.com>
2024-11-28 13:08:17 -0300

    Multiple CORS

--- server.js
+++ server.js
@@ -10,8 +10,17 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 app.use(fileUpload());
+const allowedOrigins = process.env.CORS.split(',');
+
 app.use(cors({
-  origin: process.env.CORS,
+  origin: function (origin, callback) {
+    // Permite requests sem origem (como em ferramentas como Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 
